94fcdfd8c647beae98cd7561ec5056fcfd04f96e
Mahima-Kamatwar <mahimakamatwar.785@gmail.com>
2025-08-03 12:16:59 +0530

    updated

---
+++ javascript/lessons/0.5 datatype/script.js
@@ -0,0 +1,11 @@
+// typeof() -> this function returns the data type of inputed data
+// typeof()-> in non primitive it return object
+//            in primitve it return number boolean string
+ 
+// returns a number
+console.log(typeof 23)  
+
+// returns a string
+let data = window.prompt("enter something !")
+
+console.log("type of data : " +data + " is "+typeof(data))
