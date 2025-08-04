f7862f207f6b935595febc93e7f00113fb1ad886
Mahima-Kamatwar <mahimakamatwar.785@gmail.com>
2025-08-04 14:47:15 +0530

    update

--- javascript/lessons/0.5 datatype/script.js
+++ javascript/lessons/0.5 datatype/script.js
@@ -1,9 +1,22 @@
+// datatype: JavaScript utilizes various data types to categorize the different kinds of values variables can hold. 
+// two type: primitive and non primitive
+// primitive: number string boolean
+// non primitive: object array function
+
+// console.log(typeof 10);         // "number"
+// console.log(typeof "hi");       // "string"
+// console.log(typeof true);       // "boolean"
+// console.log(typeof {});         // "object"
+// console.log(typeof []);         // "object"
+// console.log(typeof function(){}); // "function"
+
+
 // typeof() -> this function returns the data type of inputed data
-// typeof()-> in non primitive it return object
-//            in primitve it return number boolean string
+//          -> in non primitive it return object
+//          -> in primitve it return number boolean string
  
 // returns a number
-console.log(typeof 23)  
+console.log(typeof 35)  
 
 // returns a string
 let data = window.prompt("enter something !")
