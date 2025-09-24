a7a17911b253115974e91135a52a4e395a92b235
Mahima-Kamatwar <mahimakamatwar.785@gmail.com>
2025-09-24 23:18:40 +0530

    update basic node

--- node/simple.js
+++ node/simple.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+console.log(200+300)
+console.log(500-300)
+console.log(200*300)
+console.log(200/400)
+let data=123
+console.log(data)
+console.log("hello "+ "everyone")
