68af5c85c0cc47726d00cffbb931b04dde2cca2d
Mahima-Kamatwar <mahimakamatwar.785@gmail.com>
2025-11-18 22:30:21 +0530

    update

--- React/react3/src/App.jsx
+++ React/react3/src/App.jsx
@@ -1,84 +1,19 @@
-// import React, { use, useState } from 'react'
-// import Comp1 from './components/comp1'
-
-// const App = () => {
-
-//   let [toShow, setToShow] = useState(false)
-
-//   let [profile, setProfile] = useState({
-//     name: "mahima kamatwar",
-//     contact: "9022836145",
-//     email: "mahima.785@gmail.com",
-//     address: "bahadura nagpur 440024"
-//   })
-
-//   let [data, setData] = useState(123)
-
-//   const handleClick = () => {
-//     setToShow((prev) => {
-//       return !prev
-//     })
-   
-//   }
-
-//   return (
-//     <div className='p-5 m-5'>
-//       {/* <h1 className='p-6 text-6xl text-red-500 bg-yellow-300'>tailwind</h1> */}
-
-//       <Comp1 />
-
-//       {
-//         toShow ? <h1 className='bg-red-500 text-black px-5 py-3 inline absolute start-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>hiii</h1> : null
-//       }
-
-//       {
-//         profile ?
-//           <div>
-//             <table className='table border-2'>
-//               <thead>
-//                 <tr>
-//                   <th>Name</th>
-//                   <th>Contact</th>
-//                   <th>Email</th>
-//                   <th>Address</th>
-//                 </tr>
-//               </thead>
-//               <tbody>
-//                 <tr>
-//                   <td>{profile.name ? profile.name : "none"}</td>
-//                   <td>{profile.contact}</td>
-//                   <td>{profile.email}</td>
-//                   <td>{profile.address}</td>
-//                 </tr>
-//               </tbody>
-//             </table>
-//           </div> :
-//           <h1>empty profile !</h1>
-//       }
-
-//       <button onClick={handleClick} className='bg-blue-500 px-3 py-2 mt-5 font-bold text-white'>Toggle</button>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import React, { useState } from "react";
-import Comp1 from "./components/comp1";
+import React, { useState } from "react"
+import Comp1 from "./components/comp1"
 
 const App = () => {
-  const [toShow, setToShow] = useState(false);
+  const [toShow, setToShow] = useState(false)
 
   const [profile, setProfile] = useState({
     name: "mahima kamatwar",
     contact: "9022836145",
     email: "mahima5@gmail.com",
     address: "bahadura nagpur 440024",
-  });
+  })
 
   const handleClick = () => {
     setToShow((prev) => !prev);
-  };
+  }
 
   return (
     <div className="p-5 m-5">
@@ -124,7 +59,7 @@ const App = () => {
         Toggle
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
