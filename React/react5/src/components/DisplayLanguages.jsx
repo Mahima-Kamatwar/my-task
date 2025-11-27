c1347e1aa7c4b58da4802df1c6b5d5f0bc11671e
Mahima-Kamatwar <mahimakamatwar.785@gmail.com>
2025-11-27 22:46:16 +0530

    add&save

--- React/react5/src/components/DisplayLanguages.jsx
+++ React/react5/src/components/DisplayLanguages.jsx
@@ -7,32 +7,30 @@ const DisplayLanguages = (props) => {
                 props.languagesArray.map((item) => {
                     return (
                         <div key={item.id} className='item p-2 border shadow min-w-[300px] h-[300px] rounded'>
-                        <ul className='flex flex-col gap-4 p-4 bg-white rounded-2xl shadow-md'>
-  <li className='text-3xl font-extrabold text-center text-gray-700'>
-    {item.name}
-  </li>
-  <li className='bg-gradient-to-bl from-yellow-100 to-yellow-300 text-yellow-800 px-4 py-2 rounded-xl text-center font-semibold'>
-    Difficulty: {item.difficulty}
-  </li>
-  <li className='bg-gradient-to-br from-blue-100 to-blue-300 text-blue-800 px-4 py-2 rounded-xl text-center font-semibold'>
-    Duration: {item.duration}
-  </li>
-  {/* Top Right → Bottom Left ->bl */}
-  {/* Top Left -> Bottom Right ->br */}
-    <span className='font-bold'>Used For :</span>
-                <li>
-                  <ol className='ms-10 list-disc'>
-                    {item.scope.map((item, index) => {
-                      return (
-                        <li key={index}>{item}</li>
-                      )
-                    })}
-                  </ol>
-                </li>
-</ul>
-
+                            <ul className='flex flex-col gap-4 p-4 bg-white rounded-2xl shadow-md'>
+                                <li className='text-3xl font-extrabold text-center text-gray-700'>
+                                    {item.name}
+                                </li>
+                                <li className='bg-gradient-to-bl from-yellow-100 to-yellow-300 text-yellow-800 px-4 py-2 rounded-xl text-center font-semibold'>
+                                    Difficulty: {item.difficulty}
+                                </li>
+                                <li className='bg-gradient-to-br from-blue-100 to-blue-300 text-blue-800 px-4 py-2 rounded-xl text-center font-semibold'>
+                                    Duration: {item.duration}
+                                </li>
+                                {/* Top Right → Bottom Left ->bl */}
+                                {/* Top Left -> Bottom Right ->br */}
+                                <span className='font-bold'>Used For :</span>
+                                <li>
+                                    <ol className='ms-10 list-disc'>
+                                        {item.scope.map((item, index) => {
+                                            return (
+                                                <li key={index}>{item}</li>
+                                            )
+                                        })}
+                                    </ol>
+                                </li>
+                            </ul>
                         </div>
-
                     )
                 })
             }
